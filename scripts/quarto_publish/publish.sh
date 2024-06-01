6bc4d0b173bd018b5f888e5747fc7ae6bea0718a
Vijay Janapa Reddi <vj@eecs.harvard.edu>
2024-06-01 13:04:25 -0400

    Tweak to check on the correct branch

--- scripts/quarto_publish/publish.sh
+++ scripts/quarto_publish/publish.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check if the current git branch is main
+current_branch=$(git branch --show-current)
+if [ "$current_branch" != "main" ]; then
+    echo "You are not on the main branch. Please switch to the main branch to proceed."
+    exit 1
+fi
+
 # Step 1: Run quarto render
 echo "Running quarto render..."
 quarto render
@@ -16,7 +23,7 @@ python3 ./scripts/quarto_publish/gs_compress_pdf.py -i ./_book/Machine-Learning-
 if [ $? -ne 0 ]; then
     echo "PDF compression failed!"
     exit 1
-else
+else    
     echo "PDF compression completed successfully."
 fi
 
@@ -26,7 +33,7 @@ mv ./_book/ebook.pdf ./_book/Machine-Learning-Systems.pdf
 if [ $? -ne 0 ]; then
     echo "Failed to replace the original PDF!"
     exit 1
-else
+else    
     echo "PDF replaced successfully."
 fi
 
@@ -36,7 +43,7 @@ quarto publish --no-render gh-pages
 if [ $? -ne 0 ]; then
     echo "Publishing to gh-pages failed!"
     exit 1
-else
+else    
     echo "Published to gh-pages successfully."
 fi
 
