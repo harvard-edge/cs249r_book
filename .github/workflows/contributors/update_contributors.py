696bdff786b00d11cc3e16aca637dd645896fcb2
Vijay Janapa Reddi <vj@eecs.harvard.edu>
2024-12-30 13:36:42 -0500

    Fix filename errror

--- .github/workflows/contributors/update_contributors.py
+++ .github/workflows/contributors/update_contributors.py
@@ -413,7 +413,7 @@ def main(_):
     final_result = dict(
         projectName=REPO,
         projectOwner=OWNER,
-        files=["contents/frontmatter/acknowledgements/acknowledgements.qm", "README.md"],
+        files=["contents/frontmatter/acknowledgements/acknowledgements.qmd", "README.md"],
         contributors=[
             dict(
                 login=(
