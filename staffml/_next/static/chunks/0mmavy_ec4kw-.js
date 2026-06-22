ead463ecd5e74797cc3a63a3eb3f3beaf69bf589
profvjreddi <profvjreddi@users.noreply.github.com>
2026-06-22 20:25:57 +0000

    🎯 Deploy StaffML live v33 - 296d492a7073c3f3be8ac8c02c64753f69a5cd04 296d492a7073c3f3be8ac8c02c64753f69a5cd04

---
+++ staffml/_next/static/chunks/0mmavy_ec4kw-.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,82221,t=>{"use strict";let e=(0,t.i(96094).default)("arrow-left",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]);t.s(["ArrowLeft",0,e],82221)}]);
